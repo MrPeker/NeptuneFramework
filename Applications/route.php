5a73910fe00c459ae842c6ea14f80961fdd393e6
emirhanengn <almostoverr@gmail.com>
2017-01-12 19:06:20 +0200

    Add files via upload

---
+++ Applications/route.php
@@ -0,0 +1,5 @@
+<?php
+define('BASE_URL', 'http://localhost:82/neptuneframework');
+define('STYLE_DIR', BASE_URL . '/Resources/styles');
+define('SCRIPT_DIR', BASE_URL . '/Resources/scripts');
+define('UPLOADS_DIR', BASE_URL . '/Resources/uploads');
